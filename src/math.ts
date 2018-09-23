f20c5849e812bcf4d1f327425937ac4ca5eca9d5
Belleve <belleve@typeof.net>
2018-09-23 22:25:35 +0800

    fix the bundle

--- src/math.ts
+++ src/math.ts
@@ -1,3 +1,4 @@
 const math: any = require("mathjs/core").create();
+math.import(require("mathjs/lib/type/matrix"));
 math.import(require("mathjs/lib/function/algebra/solver/lusolve"));
 export default math;
